017b85b680c7cb263cde9ba43e21dbfbd778d607
boronoff <romanticboron@gmail.com>
2024-10-03 13:22:35 +0300

    done 2.3

---
+++ src/main/java/javaCourse/calculator/CalculatorServiceInterface.java
@@ -0,0 +1,13 @@
+package javaCourse.calculator;
+
+public interface CalculatorServiceInterface {
+    String hello();
+
+    String plus(int a, int b);
+
+    String minus(int a, int b);
+
+    String multiply(int a, int b);
+
+    String divide(int a, int b);
+}
---
+++ src/test/java/javaCourse/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package javaCourse.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
